ad342368bdffd47f0e2ea05ce011275567936924
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-11-14 11:01:12 -0800

    Updated

--- routes/users/userController.js
+++ routes/users/userController.js
@@ -1,37 +1,38 @@
-var bcrypt = require('bcryptjs');
-var User = require('./User');
-
+var bcrypt = require("bcryptjs");
+var User = require("./User");
 
 module.exports = {
+  createUser: async (req, res) => {
+    console.log(req.body);
 
-    createUser: async (req, res) => {
+    try {
+      let genSalt = await bcrypt.genSalt(12);
+      let hashedPassword = await bcrypt.hash(req.body.password, genSalt);
 
-        res.send(req.body);
+      let createdUser = new User({
+        email: req.body.email,
+        password: hashedPassword,
+      });
 
-        return;
+      await createdUser.save();
 
+      res.json({
+        message: "User created",
+      });
+    } catch (e) {
+      console.log(e);
+      console.log(e.code);
+      console.log(e.message);
 
-        try {
-            let createdUser = new User({
-                email: req.body.email,
-                password: req.body.password
-            });
-
-            let genSalt = await bcrypt.genSalt(12);
-            let hashedPassword = await bcrypt.hash(createUser.password, genSalt);
-
-            createdUser.password = hashedPassword;
-
-            await createdUser.save();
-
-            res.json({
-                message: "User Created"
-            });
-        } 
-        catch(e) {
-            res.status(500).json({
-                message: "Something went wrong"
-            });
-        }
-    },
+      if (e.code === 11000) {
+        res.status(409).json({
+          message: "Email is duplicate",
+        });
+      } else {
+        res.status(500).json({
+          message: "Something went wrong",
+        });
+      }
+    }
+  },
 };
